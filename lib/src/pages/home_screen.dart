eb18b44f8526fca24f59b8c6912920c968d51849
JPincay7 <jonathan54jc@live.com>
2021-10-14 10:53:25 -0500

    home screen view

--- lib/src/pages/home_screen.dart
+++ lib/src/pages/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:my_app/src/drawer/navigation_drawer.dart';
 
@@ -14,21 +16,98 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
       drawer: NavigationDrawer(),
-      body: SafeArea(child: TravelView()),
-    );
-  }
-}
-
-class TravelView extends StatelessWidget {
-  // const TravelView({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: [
-          Text('Hello World'),
-        ],
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(30),
+                ),
+              ),
+              padding: EdgeInsets.all(
+                20.0,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Find your',
+                    style: TextStyle(color: Colors.black87, fontSize: 25),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    'Inspiration',
+                    style: TextStyle(color: Colors.black, fontSize: 40.0),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(
+                      5.0,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(
+                        244,
+                        243,
+                        243,
+                        1,
+                      ),
+                      borderRadius: BorderRadius.circular(
+                        5.0,
+                      ),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: Colors.black87,
+                        ),
+                        hintText: 'Search you are looking for',
+                        hintStyle: TextStyle(color: Colors.grey, fontSize: 15),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Promo Today',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    height: 200,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: [],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
